e0d1e92fd6536b2e2455e9f25f9cadbccd2d8c6a
FOURNIER Charles <c.fournier@DIGI-LAPA222.local>
2026-02-27 09:40:42 +0100

    fix bug click

--- shared/components/FloatingContact/index.tsx
+++ shared/components/FloatingContact/index.tsx
@@ -201,7 +201,7 @@ export default function FloatingContact() {
 				</svg>
 			</motion.button>
 		</div>
-		<div className={`floating-contact__overlay ${isOpen ? ' floating-contact__overlay--open' : ''}`}  onClick={toggle} />
+		<div className={`floating-contact__overlay ${isOpen ? ' floating-contact__overlay--open' : ''}`}  {...(isOpen && {onClick: toggle})} />
 		</>
 	);
 }
