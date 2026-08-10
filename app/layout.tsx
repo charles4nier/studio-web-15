9ab8a179cb4103a8e4f7e608565d6ea20d9da8e4
FOURNIER Charles <c.fournier@DIGI-LAPA222.local>
2026-08-10 09:45:19 +0200

    Add theme white

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,7 @@ export const viewport: Viewport = {
 	width: 'device-width',
 	initialScale: 1,
 	viewportFit: 'cover',
-	themeColor: [
-		{ media: '(prefers-color-scheme: light)', color: '#ffffff' },
-		{ media: '(prefers-color-scheme: dark)', color: '#1a1a1a' }
-	]
+	themeColor: '#ffffff'
 };
 
 import '@shared/styles/index.scss';
